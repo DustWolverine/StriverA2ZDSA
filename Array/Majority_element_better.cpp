7d1c6269769a99732bff8f467187a6afa1c29d0e
DustWolverine <akashrajnitrr@gmail.com>
2025-05-02 23:45:58 +0530

    kadens algo

---
+++ Array/Majority_element_better.cpp
@@ -0,0 +1,56 @@
+#pragma GCC optimize("Ofast")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define endl "\n"
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+
+int main()
+{
+ fast_cin();
+
+#ifndef ONLINE_JUDGE
+ freopen("input.txt", "r", stdin);
+ freopen("output.txt", "w", stdout);
+#endif
+int n;cin>>n; 
+vector<int> vec;
+for(int i=0;i<n;i++){
+    int x; cin>>x; 
+    vec.push_back(x);
+}
+
+unordered_map<int,int> mpp;
+for(auto it:vec){
+    mpp[it]++;
+}
+int majority=-1;
+for(auto it:mpp){
+    if(it.second>(n/2)){
+        majority=it.first;
+        break;
+    }
+}
+
+cout<<majority<<endl;
+ return 0;
+}
---
+++ Array/Maximum_subarray_sum_brute.cpp
@@ -0,0 +1,57 @@
+#pragma GCC optimize("Ofast")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define endl "\n"
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+
+int main()
+{
+ fast_cin();
+
+#ifndef ONLINE_JUDGE
+ freopen("input.txt", "r", stdin);
+ freopen("output.txt", "w", stdout);
+#endif
+
+int n; cin>>n;
+vector<int> vec;
+for(int i=0;i<n;i++){
+    int x;cin>>x;
+    vec.push_back(x);
+}
+int maxSum=INT_MIN;
+for(int i=0;i<n;i++){
+    for(int j=i;j<n;j++){
+        int sum=0;
+        for(int k=i;k<=j;k++){
+            sum+=vec[k];
+        }
+       maxSum=max(maxSum,sum);
+    }
+}
+//? time complexity is O(n^3) and space complexity is O(1)
+cout<<maxSum<<endl;
+
+
+ return 0;
+}
